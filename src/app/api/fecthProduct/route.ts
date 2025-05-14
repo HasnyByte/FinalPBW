95066e41437cb1738c68e2d2dd9ae16a473a53af
Rezza-rmdh <rezzaramadhana09@gmail.com>
2025-05-14 13:19:28 +0700

    Initial commit

---
+++ src/app/api/fecthProduct/route.ts
@@ -0,0 +1,21 @@
+import { connectDB } from '@/../lib/mongoose';
+import Product from "../models/product.model";
+
+export async function GET(request: Request) {
+  await connectDB();
+  try {
+    const products = await Product.find({}).sort({ createdAt: -1 });
+
+    return Response.json(
+        { products }, 
+        { status: 200 }
+    );
+  } catch (error: any) {
+    console.log("Error in fetching products");
+
+    return Response.json(
+        { message: error.message }, 
+        { status: 400 }
+    );
+  }
+}
