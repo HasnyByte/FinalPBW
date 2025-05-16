708c38bce36b7838fed798742978e836d4bf6d54
iamShaira <shaira23@mhs.usk.ac.id>
2025-05-17 01:20:44 +0700

    up

---
+++ src/components/ShopCollection.tsx
@@ -0,0 +1,14 @@
+
+
+const ShopCollection = () => {
+    return (
+        <div >
+            <a href="#product">
+                <button className="mt-8 bg-black text-white px-3 py-2 rounded-xl">
+                    Shop the Collection
+                </button>
+            </a>
+        </div>
+    )
+}
+export default ShopCollection;
