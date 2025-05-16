abcc6fec1c431481d33695249580867639ce9c8d
iamShaira <shaira23@mhs.usk.ac.id>
2025-05-16 22:31:32 +0700

    up ui

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,27 @@
 import Image from "next/image";
+import ShopCollection from "@/components/ShopCollection";
 
 export default function Home() {
   return (
-    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="text-black font-black font-bold text-5xl"> PASS.PRESENT.FUTURE</div>
-        <h1 className="text-black font-light justify-center box-content md:box-border  ">
+    <div className="bg-white grid grid-rows-[20px_1fr_20px]  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] place-content-center ">
+      <main className="flex flex-col gap-[32px] row-start-2 place-items-start  ">
+        <div className="text-black  font-bold text-5xl"> PAST.PRESENT.FUTURE</div>
+        <div className="flex  col-span-2  justify-between flex-row ">
+         <h1 className="text-black font-light  max-w-md ">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
           dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
            deserunt mollit anim id est laborum
+          <ShopCollection></ShopCollection>
         </h1>
+        
+        <div>
+          <Image src="/img/Car.png" alt="Car" width={700} height={500} />
+        </div>
+         
+        </div>
+        
+
       </main>
 
 
