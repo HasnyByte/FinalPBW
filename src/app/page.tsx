23490e50c67a6e476dda74cc31a164f9b942640c
HasnyByte <zzghif@gmail.com>
2025-05-17 21:53:35 +0700

    a

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import AddForm from "@/components/AddForm";
+import Hero from "@/components/Hero";
+import ProductList from "@/components/ProductList";
+
+export default function Home() {
+    return (
+        <div className="bg-[#F8F9FA]">
+            <Hero />
+
+            <h2 className="w-full text-center text-2xl md:text-4xl font-semibold py-6 text-black">
+                All Products
+            </h2>
+            <ProductList />
+        </div>
+    );
+}
